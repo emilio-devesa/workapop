5cc377e97b62ce43cbd7764f2e34ac865c1ba32b
Emilio Devesa <edevesam@fpcoruna.afundacion.org>
2022-01-28 12:22:57 +0100

    Main, Empleado y Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Departamento {
+public class Departamento {
+
+    private int deptno;
+    private String dName;
+    private String loc;
+
+    public Departamento(int deptno, String NDepartamento, String loc) {
+        this.deptno = deptno;
+        this.dName = NDepartamento;
+        this.loc = loc;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public String getNDepartamento() {
+        return dName;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public void setNDepartamento(String NDepartamento) {
+        this.dName = NDepartamento;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
 }
