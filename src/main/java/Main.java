ea9edd25082082a4c3e03d4d97169a464241682d
Emilio Devesa <edevesam@fpcoruna.afundacion.org>
2022-02-13 22:25:27 +0100

    Main: Added Javadoc, some code-linting and bug fixes, merged extra functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,45 @@
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Un programa que modela un software de gestión de una empresa.
+ * @author Alejandro Rey Fernández
+ * @author Emilio Devesa
+ * @author Miguel Alejandro Pita Prieto
+ * @author Adrián Brey Becerra
+ * @version 0.1
+ */
 public class Main {
 
     private static final Scanner input=new Scanner(System.in);
+    private static final String empHeader="ID \t Nombre \t Puesto \t Manager \t Contratación \t Salario \t Departamento";
+    private static final String dptHeader="ID \t Nombre \t Ubicación";
 
+    /**
+     * Punto de entrada al código de ejecución del programa. Se muestra un menú con distintas opciones.
+     * @param args El String que representa los argumentos con los que el programa se puede lanzar desde consola
+     */
     public static void main(String[] args){
         try {
             HibernateUtil.buildSessionFactory();
+            menu();
         } catch (Throwable ex){
             System.out.println("No se pudo crear la sesión de conexión a la BBDD");
         }
-        menu();
     }
 
     public static void menu(){
         boolean exit=false;
         do{
-            System.out.print("""
-                    WorkaPOP
-                    1. Insertar empleado
-                    2. Borrar empleado
-                    3. Listar empleados
-                    4. Listar departamentos
-                    0. Salir
-                    Opcion?:  """);
+            System.out.print("WorkaPOP\n"+
+                    "1. Insertar empleado\n"+
+                    "2. Borrar empleado\n"+
+                    "3. Listar empleados\n"+
+                    "4. Listar departamentos\n"+
+                    "5. Mostrar relacion Jefe <-> Empleados\n"+
+                    "0. Salir\n"+
+                    "Opcion?: ");
             try {
                 int option=Integer.parseInt(input.nextLine());
                 switch(option){
@@ -34,6 +47,7 @@ public class Main {
                     case 2: deleteEmpleado(); break;
                     case 3: listEmpleados(); break;
                     case 4: listDepartamentos(); break;
+                    case 5: mostrarRelacionJefeEmpleados(); break;
                     case 0: exit=true; break;
                     default: System.out.println("Opcion no válida"); break;
                 }
@@ -43,6 +57,9 @@ public class Main {
         } while (!exit);
     }
 
+    /**
+     * Lee los datos de un nuevo Empleado y lo almacena en la base de datos
+     */
     public static void insertEmpleado(){
         Empleado emp, mgr=null;
         Integer empno=null;
@@ -89,6 +106,9 @@ public class Main {
         System.out.println("OK!");
     }
 
+    /**
+     * Lee el ID de un Empleado y lo elimina en la base de datos
+     */
     public static void deleteEmpleado(){
         System.out.print("Introducir ID de empleado: ");
         try{
@@ -105,28 +125,78 @@ public class Main {
         }
     }
 
+    /**
+     * Muestra información de los empleados
+     */
     public static void listEmpleados(){
-        System.out.println("ID \t Nombre \t Puesto \t Manager \t Contratación \t Salario \t Departamento");
-        ArrayList<Empleado> list=Empleado.listAll();
+        System.out.println(empHeader);
+        ArrayList<Empleado> list=Empleado.getAll();
         for (Empleado e: list){
-            System.out.print(e.getId() + " \t");
-            System.out.print(e.getEname() + " \t");
-            System.out.print(e.getJob() + " \t");
-            System.out.print(e.getMgr() + " \t");
-            System.out.print(e.getHireDate() + " \t");
-            System.out.print(e.getSal() + " \t");
-            System.out.print(e.getDeptno() + "\n");
+            System.out.println(e.toString());
         }
     }
 
+    /**
+     * Muestra información de los departamentos
+     */
     public static void listDepartamentos(){
-        System.out.println("ID \t Nombre \t Localización");
-        ArrayList<Departamento> list=Departamento.listAll();
+        System.out.println(dptHeader);
+        ArrayList<Departamento> list=Departamento.getAll();
         for (Departamento d: list){
-            System.out.print(d.getId()+" \t");
-            System.out.print(d.getNDepartamento()+" \t");
-            System.out.print(d.getLoc()+" \n");
+            System.out.println(d.toString());
         }
+    }
+
+    /**
+     * Submenu que permite al usuario ver el jefe de un empleado o los empleados bajo su cargo
+     */
+    public static void mostrarRelacionJefeEmpleados(){
+        Empleado empleado=null;
+        ArrayList<Empleado> listaEmpleados=null;
+        do {
+            try {
+                listEmpleados();
+                System.out.print("ID de empleado: ");
+                empleado=Empleado.getEmpleadoById(Integer.parseInt(input.nextLine()));
+                if (empleado==null){
+                    System.out.println("El empleado no existe");
+                }
+            } catch (NumberFormatException nfe){
+                System.out.print("Formato no válido. Pulse INTRO. ");
+                input.nextLine();
+            }
+        } while (empleado==null);
+        boolean exit=false;
+        do {
+            System.out.println("Empleado actual: "+empleado.getEname());
+            System.out.print("Que desea hacer?\n"+
+                    "1. Mostrar jefe\n"+
+                    "2. Mostrar empleados a su cargo\n"+
+                    "0. Volver al menu principal\n"+
+                    "Introduzca una opción: ");
+            try{
+                switch (input.nextInt()){
+                    case 1:
+                        System.out.println("Jefe: "+empleado.getMgr().getEname());
+                        break;
+                    case 2:
+                        listaEmpleados = Empleado.getEmpleadosFromManager(empleado);
+                        if (!listaEmpleados.isEmpty()) {
+                            System.out.println(empHeader);
+                            for (Empleado e: listaEmpleados){
+                                System.out.println(e.toString());
+                            }
+                        } else {
+                            System.out.println("No tiene empledos a su cargo");
+                        }
+                        break;
+                    case 0: exit=true; break;
+                    default: System.out.println("Opcion no válida"); break;
+                }
+            } catch (NumberFormatException nfe){
+                System.out.println("Opcion no válida");
+            }
+        }while (!exit);
     }
 
 }
